028f03ef60f173beb6dfdd680a3dc44189172101
meznah <s202268000@kfupm.edu.sa>
2025-11-12 11:31:32 +0300

    TODO 3

--- 6-3-express-request-data/server.js
+++ 6-3-express-request-data/server.js
@@ -107,7 +107,6 @@ LAB SETUP INSTRUCTIONS
 import express from "express";
 const app = express();
 
-
 // create server
 app.listen(3000, () => console.log("API running at http://localhost:3000"));
 
@@ -118,8 +117,12 @@ app.get("/echo", (req, res) => {
     res.json({ ok: true, name, age, msg: `Hello ${name}, you are ${age}` });
 });
 
-
 // Route params: /profile/First/Last
+app.get("/profile/:first/:last", (req, res) => {
+    const { first, last } = req.params;
+    res.json({ ok: true, fullName: `${first} ${last}` });
+});
+
 
 
 // Route param middleware example: /users/42
