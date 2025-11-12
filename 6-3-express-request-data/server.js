dd26cdfa022048a5679c34120ca013a4ab8c254b
meznah <s202268000@kfupm.edu.sa>
2025-11-12 11:41:59 +0300

    TODO 4

--- 6-3-express-request-data/server.js
+++ 6-3-express-request-data/server.js
@@ -123,9 +123,15 @@ app.get("/profile/:first/:last", (req, res) => {
     res.json({ ok: true, fullName: `${first} ${last}` });
 });
 
-
-
 // Route param middleware example: /users/42
+app.param("userId", (req, res, next, userId) => {
+    const userIdNum = Number(userId);
+    if (!Number.isInteger(userIdNum) || userIdNum <= 0)
+        return res.status(400).json({ ok: false, error: "userId must be positive number" });
+    req.userIdNum = userIdNum;
+    next();
+});
+
 
 
 // Route params: /users/:userId route
